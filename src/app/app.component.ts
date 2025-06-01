d834e52395637702a8cbebef3e2fa938b22692b6
Nate Hicks <hicksnat@unc.edu>
2025-06-01 00:10:50 -0400

    Second test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'resume-website';
+
+  constructor(private router: Router) {}
+
+  // Navigation Functions
+  goHome() {
+    this.router.navigate(['/']);
+  }
+  goAbout() {
+    this.router.navigate(['/about'])
+  }
 }
+
+
+
